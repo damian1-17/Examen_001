dc58e50e9a160ed311d148667e9976d3a44bee9e
Damián <diegodamiandelacruz@gmail.com>
2025-12-06 15:31:14 -0500

    Update build dependencies and deployment target configuration
    - Remove `appcompat` dependency and add `androidx.test` libraries for UI testing in `app/build.gradle.kts`.
    - Update deployment target selection state in `.idea/deploymentTargetSelector.xml`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
@@ -80,5 +79,8 @@ dependencies {
 
     // RecyclerView Swipe Decorator
     implementation("it.xabaras.android:recyclerview-swipedecorator:1.4")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 
 }
